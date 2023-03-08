c0420a0debd0798e34285a47e72ed52ecc1d82e0
Yago Pérez Molanes <alu0101254678@ull.edu.es>
2023-03-08 08:15:48 +0000

    Fixed little bugs in html and spec of appcomponent

--- flying-track-ft/src/app/app.component.spec.ts
+++ flying-track-ft/src/app/app.component.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-import { ServiceWorkerModule, SwUpdate } from '@angular/service-worker';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
