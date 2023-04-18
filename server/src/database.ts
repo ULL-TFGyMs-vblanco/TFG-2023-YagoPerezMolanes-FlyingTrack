dec7dc145aa327ad26210b4668db9f836de1f251
Yago Pérez Molanes <alu0101254678@ull.edu.es>
2023-04-18 17:59:56 +0100

    delete directory

--- server/src/database.ts
+++
@@ -1,8 +0,0 @@
-import  mongoose, {ConnectOptions, connect}  from 'mongoose';
-
-connect('mongodb+srv://alu0101254678:Z8aKR8cP@cluster0.6wih6s4.mongodb.net/?retryWrites=true&w=majority', 
-  { useNewUrlParser: true, 
-    useUnifiedTopology: true,
-  } as ConnectOptions)
-  .then(db => console.log('Database is connected'))
-  .catch(err => console.log(err))
