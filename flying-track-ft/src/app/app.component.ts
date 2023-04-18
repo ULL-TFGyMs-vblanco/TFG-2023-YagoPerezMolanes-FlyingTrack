bb4db2c7e9c5b340b959427ed08ff8822fac707a
Yago Pérez Molanes <alu0101254678@ull.edu.es>
2023-04-18 11:17:10 +0100

    changes

--- flying-track-ft/src/app/app.component.ts
+++ flying-track-ft/src/app/app.component.ts
@@ -1,109 +1,25 @@
-import { Component, AfterViewInit } from '@angular/core';
-import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
-import { Platform } from '@angular/cdk/platform';
-import { filter, map } from 'rxjs/operators';
-import * as L from 'leaflet';
-import { Map } from 'leaflet';
-import { LatLngExpression } from 'leaflet';
-import * as Lo from 'leaflet.locatecontrol';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div *ngIf="location">
+      Latitude: {{ location.coords.latitude }}<br>
+      Longitude: {{ location.coords.longitude }}
+    </div>
+  `
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent {
 
-  isOnline: boolean;
-  modalVersion: boolean;
-  modalPwaEvent: any;
-  modalPwaPlatform: string|undefined;
+  location: GeolocationPosition | any;
 
-  title = 'Offline-Map';
+  constructor() {}
 
-  private map: Map | any;
-  public location: GeolocationPosition | any;
-
-  private initMap(): void {
-    if (!navigator.geolocation) {
-      console.log('location is not supported');
-    }
-
-    this.map = L.map('map', {
-      center: [ 29.023152, -13.647079 ],
-      zoom: 3
-    })
-
-    const tiles = L.tileLayer("../assets/data/Canarias/{z}/{x}/{y}.png", {
-      maxZoom: 10,
-      minZoom: 7,
-    });
-
-    tiles.addTo(this.map);
-
-    
-
-    // console.log(this.map.locate({setView: true, maxZoom: 10, watch: true}));
-
-    // let marker = L.marker([ 29.023152, -13.647079 ]).addTo(this.map);
-
-    // navigator.geolocation.getCurrentPosition((position) => {
-    //   const coords = position.coords;
-    //   const latLong = [coords.latitude, coords.longitude];
-    //   console.log(
-    //     `lat: ${position.coords.latitude}, lon: ${position.coords.longitude}`
-    //   );
-
-    //   this.map = L.map('map', {
-    //     center: [ 29.023152, -13.647079 ],
-    //     zoom: 3
-    //   });
-
-    //   const tiles = L.tileLayer('./assets/data/Canarias/{z}/{x}/{y}.png', {
-    //     maxZoom: 10,
-    //     minZoom: 7,
-    //   });
-
-    //   tiles.addTo(this.map);
-
-    //   // let marker = L.marker(latLong as LatLngExpression).addTo(this.map);
-
-    //   // marker.bindPopup('<b>Hi</b>').openPopup();
-
-    //   // let popup = L.popup().setLatLng(latLong as LatLngExpression).setContent('Estoy aqui').openOn(this.map);
-    // });
-
-    // // this.watchPosition();
-
-  }
-
-
-  constructor(private platform: Platform,private swUpdate: SwUpdate) { 
-    this.isOnline = false;
-    this.modalVersion = false;
-  }
-
-  ngAfterViewInit(): void {
-    this.updateOnlineStatus();
-
-    window.addEventListener('online',  this.updateOnlineStatus.bind(this));
-    window.addEventListener('offline', this.updateOnlineStatus.bind(this));
-
-    if (this.swUpdate.isEnabled) {
-      this.swUpdate.versionUpdates.pipe(
-        filter((evt: any): evt is VersionReadyEvent => evt.type === 'VERSION_READY'),
-        map((evt: any) => {
-          console.info(`currentVersion=[${evt.currentVersion} | latestVersion=[${evt.latestVersion}]`);
-          this.modalVersion = true;
-        }),
-      );
-    } 
-    this.loadModalPwa();
-
+  ngOnInit() {
     if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/geolocation-worker.js').then(registration => {
-        console.log('Service worker registered:', registration);
-        navigator.serviceWorker.controller?.postMessage('getLocation');
+      navigator.serviceWorker.ready.then(registration => {
+        console.log('Service worker ready:', registration);
+        registration.active?.postMessage('getLocation');
       }).catch(error => {
         console.error('Service worker registration failed:', error);
       });
@@ -118,71 +34,6 @@ export class AppComponent implements AfterViewInit {
         console.error('Unable to retrieve location:', event.data.error);
       }
     });
-
-
-    this.initMap();
   }
 
-  private updateOnlineStatus(): void {
-    this.isOnline = window.navigator.onLine;
-    console.info(`isOnline=[${this.isOnline}]`);
-  }
-
-  public updateVersion(): void {
-    this.modalVersion = false;
-    window.location.reload();
-  }
-
-  public closeVersion(): void {
-    this.modalVersion = false;
-  }
-
-  private loadModalPwa(): void {
-    if (this.platform.ANDROID) {
-      window.addEventListener('beforeinstallprompt', (event: any) => {
-        event.preventDefault();
-        this.modalPwaEvent = event;
-        this.modalPwaPlatform = 'ANDROID';
-      });
-    }
-
-    if (this.platform.IOS && this.platform.SAFARI) {
-      const isInStandaloneMode = ('standalone' in window.navigator) && ((<any>window.navigator)['standalone']);
-      if (!isInStandaloneMode) {
-        this.modalPwaPlatform = 'IOS';
-      }
-    }
-  }
-
-  public addToHomeScreen(): void {
-    this.modalPwaEvent.prompt();
-    this.modalPwaPlatform = undefined;
-  }
-
-  public closePwa(): void {
-    this.modalPwaPlatform = undefined;
-  }
-
-  watchPosition() {
-    let desLat = 0;
-    let desLon = 0;
-    let id = navigator.geolocation.watchPosition(
-      (position) => {
-        console.log(
-          `lat: ${position.coords.latitude}, lon: ${position.coords.longitude}`
-        );
-        if (position.coords.latitude === desLat) {
-          navigator.geolocation.clearWatch(id);
-        }
-      },
-      (err) => {
-        console.log(err);
-      },
-      {
-        enableHighAccuracy: false,
-        timeout: 100000,
-        maximumAge: 0,
-      }
-    );
-  }
 }
--- flying-track-ft/src/app/app.module.ts
+++ flying-track-ft/src/app/app.module.ts
@@ -12,12 +12,7 @@ import { ServiceWorkerModule } from '@angular/service-worker';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: !isDevMode(),
-      // Register the ServiceWorker as soon as the application is stable
-      // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
-    })
+    ServiceWorkerModule.register('/geolocation-worker.js', { enabled:  !isDevMode()}),
   ],
   providers: [],
   bootstrap: [AppComponent]
